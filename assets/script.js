56944af9492ec7e76a97f0d346787ca08e51a20f
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-03-07 10:59:47 -0800

    Started design and created pseudo code.

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Beginning of Code
+
+
+// Create Questions
+
+
+// Create Submit Buttons
+
+
+// Create time penalizations for wrong answers (take time away)
+
+
+// Make a new quesiton come up when the sumbit button is clicked
+
+
+// 0 out timer when all questions are answered
+
+
+// Create "Game Over" message
+
+
+// Create function to save score and initials
