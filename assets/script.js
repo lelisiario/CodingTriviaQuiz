72aa108487bdc0853d75623d90ecb889dddc2847
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-03-07 23:41:32 -0800

    Added code and created an issue with the answer  not selecting.

--- assets/script.js
+++ assets/script.js
@@ -51,11 +51,94 @@ const questions = [
 
 // Create Submit Buttons
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startTrivia() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.Answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    };
+}
+
+function selectAnswer(e) {
+    const selectBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add ("correct");
+        score++;
+    }
+    else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML.HTML = 'Your Score ${score} out of ${questions.length}|';
+    nextButton.innerHRTML = "Play Again";
+    nextButton.style.display = "Block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < question.length) {
+        showQuestion(); 
+    }
+    else{
+            showScore();
+            }
+        }
+
+
+nextButton.addEventListener("click", ()=>
+{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else {
+        startTrivia();
+    }
+})
 
 
 
+startTrivia();
 
 
 
